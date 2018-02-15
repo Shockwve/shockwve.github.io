01ca4451ed10866fa91d210d83edb56a049fcac9
Shockwve <shockwaveOO9@yahoo.com>
2018-02-14 20:11:28 -0500

    Update hat-bingo-goals.js

--- script/hat-bingo-goals.js
+++ script/hat-bingo-goals.js
@@ -1,7 +1,7 @@
 var bingoList = [];
 
 bingoList[1] = [
-  { name: "Obtain 2 hats", types: ["hat"] },
+  { name: "Craft 2 hats", types: ["hat"] },
   { name: "Open 4 Chests in Mafia Town", types: ["chest"] },
   { name: "Activate and complete 2 Pon Party Balls", types: ["pon"] },
 ];
@@ -16,17 +16,16 @@ bingoList[3] = [
 ];
 bingoList[4] = [
   { name: "Collect 2 Badges", types: ["badge"] },
-  { name: "Obtain 3 hats", types: ["hat"] },
+  { name: "Craft 3 Hats", types: ["hat"] },
   { name: "Save 3 men from the mafia's beating", types: [""] },
 ];
 bingoList[5] = [
-  { name: "Collect the Scooter Badge", types: ["badge"] },
   { name: "Collect 3 Badges", types: ["badge"] },
   { name: "Juice Bar? Selfie" , types: ["selfie"] },
+  { name: "Open 3 chests in Subcon", types: ["chest"] },
 ];
 bingoList[6] = [
   { name: "Repair 1 Relic" , types: ["relic"] },
-  { name: "Collect a net total of 500 Pons", types: ["pon"] },
   { name: "Collect 3 roullette tokens", types: ["relic"] },
 ];
 bingoList[7] = [
@@ -40,9 +39,9 @@ bingoList[8] = [
   { name: "Collect all comic pieces from one pink time rift", types: ["rift"] },
 ];
 bingoList[9] = [
-  { name: "Complete A Murder On Owl Express with collecting 0 evidence", types: ["act"] },
-  { name: "Repair 3 Relics", types: ["relic"] },
-  { name: "Gallery Time Rift", types: ["rift"] },
+  { name: "Grab all 4 golden tickets in Golden Vault", types: ["act"] },
+  { name: "Collect the Scooter Badge", types: ["badge"] },
+  { name: "Complete all 4 endorsements in Picture Perfect", types: ["act"] },
 ];
 bingoList[10] = [
   { name: "Complete 2 pink rifts", types: ["rift"] },
@@ -50,80 +49,79 @@ bingoList[10] = [
   { name: "Relic at the top of Subcon's Tree", types: ["relic"] },
 ];
 bingoList[11] = [
-  { name: "Craft 4 hats", types: ["hat"] },
-  { name: "Buy the Mumble Badge", types: ["badge"] },
-  { name: "Collect 1000 Pons", types: ["pon"] },
+  { name: "Open 3 chests in Dead Bird Studio", types: ["hat"] },
+  { name: "Repair 3 Relics", types: ["relic"] },
+  { name: "Collect 3 yarns in subcon", types: ["yarn"] },
 ];
 bingoList[12] = [
-  { name: "Collect 3 yarns in subcon", types: ["yarn"] },
+  { name: "Get all photo album pieces from DBS Rift", types: ["rift"] },
+  { name: "Selfie at the top of floating stonehenge", types: ["selfie"] },
+];
+bingoList[13] = [
+  { name: "Gather all 6 evidence pieces in Murder on the Owl Express", types: ["yarn"] },
   { name: "Storytime Selfie", types: ["selfie"] },
   { name: "Get into the Mailroom without the Ice Hat", types: [""] },
 ];
-bingoList[13] = [
-  { name: "Parade Selfie", types: ["selfie"] },
-  { name: "Open 3 chests in Subcon", types: ["chest"] },
-  { name: "Defeat Snatcher", types: ["boss"] },
-];
 bingoList[14] = [
+  { name: "Parade Selfie", types: ["selfie"] },
+   { name: "Vanessa Selfie", types: ["selfie"] },
+  { name: "Get all 10 Time Pieces from Mafia Town", types: ["tp"] },
+];
+bingoList[15] = [
   { name: "Destroy 3 tracker eyeballs in Subcon forest", types: ["etc"] },
   { name: "Owl Express Rift", types: ["rift"] },
   { name: "Train rush with 100+ seconds left", types: ["act"] },
-];
-bingoList[15] = [
-  { name: "Complete 3 painting burning rituals in Subcon", types: ["etc"] },
-  { name: "Complete The Big Parade", types: ["act"] },
-  { name: "Collect 5 roullette tokens", types: ["relic"] },
+   { name: "Beat 2 Bosses", types: ["boss"] },
 ];
 bingoList[16] = [
-  { name: "Beat any finale excluding fake finale", types: ["boss"] },
-  { name: "Beat 2 Bosses", types: ["boss"] },
-  { name: "Kill the Toilet of Doom", types: ["boss"] },
+  { name: "Complete 3 painting burning rituals in Subcon", types: ["etc"] },
+  { name: "Collect 5 roullette tokens", types: ["relic"] },
+  { name: "Craft 4 hats", types: ["hat"] },
 ];
 bingoList[17] = [
-  { name: "Repair all Relics", types: ["selfie"] },
-  { name: "Vanessa Selfie", types: ["selfie"] },
-  { name: "The Moon Rift", types: ["rift"] },
+  { name: "Beat any finale excluding fake finale", types: ["boss"] },
+  { name: "Kill the Toilet of Doom", types: ["boss"] },
+  { name: "Complete The Big Parade", types: ["act"] },
 ];
 bingoList[18] = [
-  { name: "Make the Conductor win the award", types: ["botb"] },
-  { name: "Make DJ Grooves win the award", types: ["botb"] },
-  { name: "Alpine Skyline Pink Rift", types: ["botb"] },
+   { name: "Collect all photo album pieces from Sleepy Subcon rift", types: ["rift"] },
+  { name: "Create a relic in The Lab", types: [""] },
 ];
 bingoList[19] = [
+  { name: "Repair all Relics", types: [""] },
   { name: "1000 points in Battle of the Birds Feud", types: ["botb"] },
-  { name: "Collect 15 Time Pieces", types: ["tp"] },
-  { name: "Get the chest at the Goat Refinery", types: ["alpine"] },
-  { name: "Curly Tail Trail Rift", types: ["rift"]}
+  { name: "The Moon Rift", types: ["rift"] },
 ];
 bingoList[20] = [
-  { name: "Get the chest at Purrloined Village", types: ["alpine"] },
-  { name: "Blow the horn in Mystifying Time Mesa", types: ["alpine"] },
-  { name: "Selfie riding the Windmill in Alpine", types: ["alpine"] },
+ { name: "Make DJ Grooves win the award", types: ["botb"] },
+ { name: "Make the Conductor win the award", types: ["botb"] },
+  { name: "Defeat Snatcher", types: ["boss"] },
 ];
 bingoList[21] = [
-  { name: "Craft Time Stop Hat", types: ["hat"] },
-  { name: "Complete 1 act from Alpine Skyline", types: ["act"] },
-  { name: "1 Rift from each Chapter", types: ["rift, tp"] },
+  { name: "Get the chest at Purrloined Village", types: ["alpine"] },
+  { name: "Collect 15 Time Pieces", types: ["tp"] },
 ];
 bingoList[22] = [
-  { name: "Birdie Selfie", types: ["selfie"] },
-  { name: "Collect 20 time pieces", types: ["tp"] },
-  { name: "3 Acts from each Chapter", types: ["act, tp"] },
+ { name: "Get the chest at the Goat Refinery", types: ["alpine"] },
+ { name: "Selfie with a goat", types: ["selfie"] },
+ { name: "Green Screen Selfie in DBS Basement", types: ["selfie"] },
 ];
 bingoList[23] = [
-  { name: "All Acts in Skyline", types: ["act"] },
-  { name: "Collect 25 time pieces", types: ["tp"] },
-  { name: "Golden Girl Selfie", types: ["selfie"] },
+  { name: "1 Rift from each Chapter", types: ["rift, tp"] },
+  { name: "Get all the photo album pieces from Alpine Skyline Rift", types: ["rift"] },
+  { name: "Get the chest at Purrloined Village", types: ["alpine"] },
 ];
 bingoList[24] = [
-  { name: "Crybaby - Run away crying from Moustache Girl", types: ["etc"]},
-  { name: "Final Boss Fight Selfie", types: ["selfie"] },
-  { name: "Take a number in Time's End", types: ["etc"] },
+  { name: "Blow the horn in Mystifying Time Mesa", types: ["alpine"] },
+  { name: "Birdie Selfie", types: ["selfie"] },
+  { name: "Craft Time Stop Hat", types: ["hat"] },
+  { name: "Selfie with a ghost goat in The Twilight Bell", types: ["selfie"] },
 ];
 bingoList[25] = [
-  { name: "Collect 30 time pieces", types: ["tp"] },
-  { name: "All blue time rifts", types: ["rift"] },
-  { name: "Beat every boss", types: ["boss"] },
+  { name: "Curly Tail Trail Rift", types: ["tp"] },
+  { name: "Complete 1 act from Alpine Skyline", types: ["rift"] },
+  { name: "Selfie riding the Windmill in Alpine", types: ["alpine"] },
+  { name: "Complete all acts in Battle of the Birds", types: ["botb"] },
 ];
 
 $(function() { srl.bingo(bingoList, 5); });
